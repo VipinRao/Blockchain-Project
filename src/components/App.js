f18b06d76f28f40c20814ea2b258b9a12c90a7a7
Vipin Yadav <vipinyadav1041998@gmail.com>
2020-03-28 19:37:23 +0530

    comments added to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -36,15 +36,15 @@ class App extends Component {
   async loadBlockchainData() {
     const web3 = window.web3
     // Load account
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
-    const networkId = await web3.eth.net.getId()
-
-    const networkData = Healthcare.networks[networkId]
+    const accounts = await web3.eth.getAccounts() // get all acocounts 
+    this.setState({ account: accounts[0] })  // using the cuurent active account .... make account address accessable to calling obejct
+    const networkId = await web3.eth.net.getId()  // netork id retrieve
+    // Healthcare contains smart contract named healthcare  properties in json format
+    const networkData =  await Healthcare.networks[networkId] // get all network where smart contract is deployed
     if(networkData){
       const healthcare = await web3.eth.Contract(Healthcare.abi, networkData.address)
-      this.setState({ healthcare })
-      this.state.Hospital_Details = await this.state.healthcare.methods.HospitalDetail(this.state.account).call({from : this.state.account})
+      this.setState({ healthcare }) //same as {healthcare : healthcare} -> healthcare can be accessed from all-over app.js 
+      this.state.Hospital_Details = await this.state.healthcare.methods.HospitalDetail(this.state.account).call({from : this.state.account})  // from : this.state.acoount is the address of node calling smart constract and the other addreess is the argument
       this.state.owner = await this.state.healthcare.methods.checkOwner().call({from: this.state.account})
       this.setState({loading : false})
        }
@@ -54,7 +54,7 @@ class App extends Component {
 
   }
 
-    constructor(props) {
+    constructor(props) { 
     super(props)
     this.state = {
       account: '',
